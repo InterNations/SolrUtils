f75c54376e99815f9c58a3babdffddb876974210
Fehér Zoltán <feher.zoltan@arkon.hu>
2016-07-13 10:12:30 +0200

    precision is good in hhvm

--- tests/InterNations/Component/Solr/Tests/UtilTest.php
+++ tests/InterNations/Component/Solr/Tests/UtilTest.php
@@ -104,11 +104,7 @@ class UtilTest extends TestCase
 
     public function testSanitizing_ScientificNotationDoesNotIntroduceMinusChar()
     {
-        if (defined('HHVM_VERSION')) {
-            $this->assertSame('0.00002099999992', Util::sanitize(2.1E-5));
-        } else {
-            $this->assertSame('0.00002100000000', Util::sanitize(2.1E-5));
-        }
+        $this->assertSame('0.00002100000000', Util::sanitize(2.1E-5));
     }
 
     /** @dataProvider getEscapingStrings */
